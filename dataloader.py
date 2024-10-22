37cac78e4b67e8561dd62a25549d8acbb8fd6984
Turningl <60030314+Turningl@users.noreply.github.com>
2024-10-22 21:27:48 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -13,15 +13,18 @@ lg = rdkit.RDLogger.logger()
 lg.setLevel(rdkit.RDLogger.CRITICAL)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root', type=str, default='MoleculeNet', help='MoleculeNet, DrugBank, BIOSNAP, LITPCBA, CoCrystal')
+parser.add_argument('--root', type=str, default='MoleculeNet', choices=['MoleculeNet, DrugBank, BIOSNAP, LITPCBA, CoCrystal'],
+                        help='choose the dataset, start with the path to dataset dir.')
 parser.add_argument('--dataset', default='tox21', type=str)
-parser.add_argument('--split', default='smi', type=str, help='smi, smi1, smi2, random') #  MoleculeNet and LITPCBA we use smi, DrugBank, BIOSNAP and CoCrystal we use smi1, smi1
-parser.add_argument('--seed', default=4567, type=int, help='1234, 4567, 7890')
-parser.add_argument('--device', default='cuda', type=str)
+parser.add_argument('--split', default='smi', type=str, choices=['smi, smi1, smi2, random'],
+                    help='Choose a data splitting method.')  #  MoleculeNet and LITPCBA we use smi, DrugBank, BIOSNAP and CoCrystal we use smi1, smi1
+parser.add_argument('--seed', default=4567, type=int, choices=[1234, 4567, 7890],
+                    help='Initialize random number seeds for model training and data splitting.')
+parser.add_argument('--device', default='cuda', type=str,
+                    choices=['cuda', 'cpu'])
 args = parser.parse_args()
 print(args)
 
 exec("dataset = {}Dataset('./dataset/{}', '{}', '{}', {})".format(args.root, args.root, args.dataset, args.split, args.seed))
 # dataset = MolDataset(root='../dataset/' + args.root, name=args.dataset, split_seed=args.seed)
 print('the dataset name: {}, the mol size: {}.'.format(args.dataset, len(dataset)))
-
