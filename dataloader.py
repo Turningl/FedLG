4553037d989874b644ea54e2f095c3c397ca4902
Turningl <60030314+Turningl@users.noreply.github.com>
2024-11-21 21:32:55 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -13,10 +13,10 @@ lg = rdkit.RDLogger.logger()
 lg.setLevel(rdkit.RDLogger.CRITICAL)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root', type=str, default='MoleculeNet', choices=['MoleculeNet, DrugBank, BIOSNAP, LITPCBA, CoCrystal'],
+parser.add_argument('--root', type=str, choices=['MoleculeNet, DrugBank, BIOSNAP, LITPCBA, CoCrystal'],
                         help='choose the dataset, start with the path to dataset dir.')
 parser.add_argument('--dataset', default='tox21', type=str)
-parser.add_argument('--split', default='smi', type=str, choices=['smi, smi1, smi2, random'],
+parser.add_argument('--split', type=str, choices=['smi, smi1, smi2, random'],
                     help='Choose a data splitting method.')  #  MoleculeNet and LITPCBA we use smi, DrugBank, BIOSNAP and CoCrystal we use smi1, smi1
 parser.add_argument('--seed', default=4567, type=int, choices=[1234, 4567, 7890],
                     help='Initialize random number seeds for model training and data splitting.')
