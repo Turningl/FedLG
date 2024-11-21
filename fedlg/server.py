4a12b78a9b96fca9bca15015156c9c515186295d
Turningl <60030314+Turningl@users.noreply.github.com>
2024-11-21 21:18:25 +0800

    Update server.py

--- fedlg/server.py
+++ fedlg/server.py
@@ -6,14 +6,15 @@ import torch
 import numpy as np
 import scipy as sp
 from utils.lanczos import Lanczos
-from utils.save_func import print_init_alg_info, print_average_info
+from utils.saveutils import print_init_alg_info, print_average_info
+
 
 def add_weights(num_vars, model_state, agg_model_state):
-    for i in range(num_vars):
-        if not len(agg_model_state):
-            a = torch.unsqueeze(model_state[i], 0)
-        else:
-            b = torch.cat([agg_model_state[i], torch.unsqueeze(model_state[i], 0)], 0)
+    # for i in range(num_vars):
+    #     if not len(agg_model_state):
+    #         a = torch.unsqueeze(model_state[i], 0)
+    #     else:
+    #         b = torch.cat([agg_model_state[i], torch.unsqueeze(model_state[i], 0)], 0)
     return [torch.unsqueeze(model_state[i], 0)
             if not len(agg_model_state) else torch.cat([agg_model_state[i],
                                                         torch.unsqueeze(model_state[i], 0)], 0)
@@ -120,7 +121,7 @@ class FedSGD:
 
 
 class FedLG:
-    def __init__(self, proj_dims, lanczos_iter, device='cuda'):
+    def __init__(self, proj_dims, lanczos_iter, device='cuda', comm_optimization=None):
         self.proj_dims = proj_dims
         self.lanczos_iter = lanczos_iter
         self.device = device
@@ -135,7 +136,9 @@ class FedLG:
 
         self.num_vars = None
         self.shape_vars = None
-        self.Vks = None
+
+        self.comm_optimization = comm_optimization
+        self.open_access_model_states = None
         self.means = None
 
     def aggregate(self, eps, model_state, is_open_access):
@@ -164,21 +167,21 @@ class FedLG:
         return M - mean, mean.flatten()
 
     def __eigen_by_lanczos(self, mat):
-        T, V = Lanczos(mat, self.lanczos_iter)  # getting a tridiagonal matrix T and an orthogonal matrix V
+        Tri_Mat, Orth_Mat = Lanczos(mat, self.lanczos_iter)  # getting a tridiagonal matrix T and an orthogonal matrix V
 
         # T_evals_, T_evecs_ = np.linalg.eig(T)  # calculating the eigenvalues and eigenvectors of a tridiagonal matrix
-        T_evals, T_evecs = sp.sparse.linalg.eigsh(T, k=2, which='LM')
+        T_evals, T_evecs = sp.sparse.linalg.eigsh(Tri_Mat, k=2, which='LM')
 
         idx = T_evals.argsort()[-1: -(self.proj_dims + 1): -1]  # finding the index of the largest element in the eigenvalue array T evals
 
-        Vk = np.dot(V.T, T_evecs[:, idx])  # the eigenvector corresponding to the maximum eigenvalue is projected into the new eigenspace
+        proj_eigenvecs = np.dot(Orth_Mat.T, T_evecs[:, idx])  # the eigenvector corresponding to the maximum eigenvalue is projected into the new eigenspace
 
-        if Vk.size >= 2:
-            Vk = torch.from_numpy(Vk).to(torch.float32).to(self.device).squeeze()
+        if proj_eigenvecs.size >= 2:
+            proj_eigenvecs = torch.from_numpy(proj_eigenvecs).to(torch.float32).to(self.device).squeeze()
         else:
-            Vk = torch.from_numpy(Vk).to(torch.float32).to(self.device).squeeze(0)
+            proj_eigenvecs = torch.from_numpy(proj_eigenvecs).to(torch.float32).to(self.device).squeeze(0)
 
-        return Vk
+        return proj_eigenvecs
 
     def __lanczos_graph_proj(self):
         if len(self.__private_institutional_model_state):
@@ -194,15 +197,55 @@ class FedLG:
 
             for i in range(self.num_vars):
                 open_access_model_state, mean = self.__standardize(self.__open_access_model_state[i].T)
-                Vk = self.__eigen_by_lanczos(open_access_model_state.T)
-                mean_proj_priv_model_state[i] = torch.mul(Vk, torch.dot(Vk.T, (mean_priv_model_state[i] - mean))) + mean
+                proj_eigenvecs = self.__eigen_by_lanczos(open_access_model_state.T)
+                mean_proj_priv_model_state[i] = torch.mul(proj_eigenvecs, torch.dot(proj_eigenvecs.T, (mean_priv_model_state[i] - mean))) + mean
                 mean_model_state[i] = ((mean_proj_priv_model_state[i] * sum(self.__private_institutional_eps) + mean_pub_model_state[i] * sum(self.__open_access_eps)) / sum(self.__private_institutional_eps + self.__open_access_eps)).reshape( self.shape_vars[i])
 
             return mean_model_state
 
+    def __lanczos_graph_proj_communication_optimization(self, warmup):
+        if len(self.__private_institutional_model_state):
+            private_institutional_weights = (torch.Tensor(self.__private_institutional_eps) / sum(self.__private_institutional_eps)).view(self.__num_privacy_institution_databases, 1).to(self.device)
+            open_access_weights = (torch.Tensor(self.__open_access_eps) / sum(self.__open_access_eps)).view(self.__num_open_access_databases, 1).to(self.device)
+
+            mean_priv_model_state = [
+                torch.sum(self.__private_institutional_model_state[i].to(self.device) * private_institutional_weights, 0) / torch.sum(private_institutional_weights) for i in range(self.num_vars)]
+            mean_pub_model_state = [
+                torch.sum(self.__open_access_model_state[i].to(self.device) * open_access_weights, 0) / torch.sum(open_access_weights) for i in range(self.num_vars)]
+            mean_proj_priv_model_state = [0] * self.num_vars
+            mean_model_state = [0] * self.num_vars
+
+            open_access_model_states = []
+            means = []
+
+            if warmup:
+                for i in range(self.num_vars):
+                    open_access_model_state, mean = self.__standardize(self.__open_access_model_state[i].T)
+                    proj_eigenvecs = self.__eigen_by_lanczos(open_access_model_state.T)
+                    mean_proj_priv_model_state[i] = torch.mul(proj_eigenvecs, torch.dot(proj_eigenvecs.T, (mean_priv_model_state[i] - mean))) + mean
+                    mean_model_state[i] = ((mean_proj_priv_model_state[i] * sum(self.__private_institutional_eps) + mean_pub_model_state[i] * sum(self.__open_access_eps))
+                                           / sum(self.__private_institutional_eps + self.__open_access_eps)).reshape(self.shape_vars[i])
+
+                    open_access_model_states.append(open_access_model_state)
+                    means.append(mean)
+            else:
+                for i in range(self.num_vars):
+                    mean_model_state[i] = ((mean_proj_priv_model_state[i] * sum(self.__private_institutional_eps) + mean_pub_model_state[i] * sum(self.__open_access_eps))
+                                           / sum(self.__private_institutional_eps + self.__open_access_eps)).reshape(self.shape_vars[i])
+                    open_access_model_state, mean = self.__standardize(self.__open_access_model_state[i].T)
+                    open_access_model_states.append(open_access_model_state)
+                    means.append(mean)
+
+            self.open_access_model_states = open_access_model_states
+            self.means = means
+            return mean_model_state
+
     def average(self):
         # mean_updates = None
-        mean_updates = self.__lanczos_graph_proj()
+        if self.comm_optimization:
+            mean_updates = self.__lanczos_graph_proj_communication_optimization(warmup=(self.open_access_model_states is None))
+        else:
+            mean_updates = self.__lanczos_graph_proj()
 
         self.__num_open_access_databases = 0
         self.__num_privacy_institution_databases = 0
@@ -214,7 +257,6 @@ class FedLG:
         self.__private_institutional_eps = []
 
         return mean_updates
-
 
 class GlobalServer:
     def __init__(self, model, args):
@@ -231,6 +273,7 @@ class GlobalServer:
         self.beta1 = args.beta1
         self.beta2 = args.beta2
         self.lr = args.lr
+        self.comm_optimization = args.comm_optimization
         # self.warmup = args.warmup
 
         self.num_vars = None
@@ -248,16 +291,19 @@ class GlobalServer:
     def init_global_model(self):
         return self.model
 
+    def fetch_comm_optimization(self):
+        return self.__alg.open_access_model_states, self.__alg.means
+
     @print_init_alg_info('Initialization')
     def init_alg(self, alg):
-        if alg == 'FedAvg' or alg == 'FedProx' or alg == 'FedChem':
+        if alg == 'FedAvg' or alg == 'FedProx' or alg == 'FLIT':
             self.__alg = FedAvg()
         elif alg == 'FedSGD':
             self.__alg = FedSGD(self.lr)
         elif alg == 'FedAdam':
             self.__alg = FedAdam(beta1=self.beta1, beta2=self.beta2, epsilon=self.__epsilons, lr=self.lr, device=self.device)
         elif alg == 'FedLG':
-            self.__alg = FedLG(self.proj_dims, self.lanczos_iter, self.device)
+            self.__alg = FedLG(self.proj_dims, self.lanczos_iter, self.device, self.comm_optimization)
         else:
             raise ValueError('\nSelect an algorithm to get the aggregated model.\n')
 
@@ -267,7 +313,7 @@ class GlobalServer:
         if alg == 'FedLG':
             self.__alg.aggregate(self.__epsilons[participant], model_state,
                                  is_open_access=True if (participant in self.open_access) else False)
-        elif alg == 'FedAvg' or alg == 'FedProx' or alg == 'FedChem':
+        elif alg == 'FedAvg' or alg == 'FedProx' or alg == 'FLIT':
             self.__alg.aggregate(model_state)
         elif alg == 'FedSGD':
             self.__alg.aggregate(model_state)
