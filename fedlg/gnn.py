2de90b279473d72b9c9a7823f56f91348614edd4
Turningl <60030314+Turningl@users.noreply.github.com>
2024-11-22 11:56:42 +0800

    Update

--- fedlg/gnn.py
+++ fedlg/gnn.py
@@ -399,10 +399,10 @@ class Linear_BatchNorm1d(nn.Module):
 #                                          nn.ReLU(),
 #                                          nn.Dropout(p=args.dropout))
 #
-#         self.norm1 = nn.Sequential(Linear_BatchNorm1d(4 * args.hidden_size, s2y4.png),
+#         self.norm1 = nn.Sequential(Linear_BatchNorm1d(4 * args.hidden_size, 1),
 #                                    nn.Dropout(p=args.dropout))
 #
-#         self.norm2 = nn.Sequential(Linear_BatchNorm1d(2 * args.hidden_size, s2y4.png),
+#         self.norm2 = nn.Sequential(Linear_BatchNorm1d(2 * args.hidden_size, 1),
 #                                    nn.Dropout(p=args.dropout))
 #
 #         self.gru1 = nn.GRUCell(3 * args.hidden_size, args.hidden_size)
@@ -422,19 +422,19 @@ class Linear_BatchNorm1d(nn.Module):
 #         node = self.node_linear(x)
 #         bond = self.edge_linear(edge_attr)
 #
-#         # neighour_node = node[edge_index[s2y4.png]]
-#         neighour = torch.cat([bond, node[edge_index[s2y4.png]], node[edge_index[s2y4.png]] + bond - node[edge_index[s2y4.png]] * bond], dim=-s2y4.png)
-#         feature_concat = torch.cat([node[edge_index[0]], neighour], dim=-s2y4.png)
-#         attention_weight = nn.functional.softmax(nn.functional.leaky_relu(self.norm1(feature_concat)), dim=s2y4.png)
+#         # neighour_node = node[edge_index[1]]
+#         neighour = torch.cat([bond, node[edge_index[1]], node[edge_index[1]] + bond - node[edge_index[1]] * bond], dim=-1)
+#         feature_concat = torch.cat([node[edge_index[0]], neighour], dim=-1)
+#         attention_weight = nn.functional.softmax(nn.functional.leaky_relu(self.norm1(feature_concat)), dim=1)
 #         context = scatter_add(torch.mul(attention_weight, neighour), index=edge_index[0], dim=0, dim_size=x.shape[0])
 #         node = self.gru1(nn.functional.elu(context), node)
 #         node = self.attention(node, edge_index, edge_attr, batch)
 #
-#         # supernode_num = batch.max() + s2y4.png
-#         supernode = scatter_add(node, batch, dim=0, dim_size=batch.max() + s2y4.png)
-#         e1 = nn.functional.leaky_relu(self.norm2(torch.cat([supernode[batch], node], dim=-s2y4.png)))
-#         attention_weight_sa = nn.functional.softmax(e1, dim=-s2y4.png)
-#         context_sa = scatter_add(torch.mul(attention_weight_sa, node), index=batch, dim=0, dim_size=batch.max() + s2y4.png)
+#         # supernode_num = batch.max() + 1
+#         supernode = scatter_add(node, batch, dim=0, dim_size=batch.max() + 1)
+#         e1 = nn.functional.leaky_relu(self.norm2(torch.cat([supernode[batch], node], dim=-1)))
+#         attention_weight_sa = nn.functional.softmax(e1, dim=-1)
+#         context_sa = scatter_add(torch.mul(attention_weight_sa, node), index=batch, dim=0, dim_size=batch.max() + 1)
 #         update = self.output(self.gru2(nn.functional.elu(context_sa), supernode))
 #
 #         return update
