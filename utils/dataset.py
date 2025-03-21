9d71bcfd5345a26df7e3b99fe2ff9c72a87e4d5c
Turningl <60030314+Turningl@users.noreply.github.com>
2025-03-21 17:16:49 +0800

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -776,6 +776,16 @@ class CoCrystalDataset(InMemoryDataset):
     def related_title(self):
         return 'CoCrystal'
 
+class HighConfidenceDataset(torch.utils.data.Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
 
 if __name__ == '__main__':
     import os
